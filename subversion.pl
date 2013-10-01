a770a990b4b1ade9188baf993483f837a556a754
syardumi <steveyardumian@gmail.com>
2013-10-01 15:45:39 -0400

    Add token and domain for Nxtbook

--- subversion.pl
+++ subversion.pl
@@ -41,8 +41,8 @@ use JSON;
 # Customizable vars. Set these to the information for your team
 #
 
-my $opt_domain = "foo.slack.com"; # Your team's domain
-my $opt_token = ""; # The token from your SVN services page
+my $opt_domain = "nxtbookdev.slack.com"; # Your team's domain
+my $opt_token = "3UjMdmQUt2SZ5R5rg91PtFyc"; # The token from your SVN services page
 
 
 #
